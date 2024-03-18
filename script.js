6ddb054673fe0505b6dbf3c7f9748daca069ca53
plastikup <zachk3yata2@gmail.com>
2024-03-18 15:58:31 -0400

    2024-03-18 info class progress

--- script.js
+++ script.js
@@ -2,10 +2,51 @@
 
 /* ctx initialization */
 const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d', { alpha: false });
 const canvasScaleFactor = 2;
+let canvasWidth, canvasHeight;
 
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+function newCanvasSize() {
+	canvas.width = innerWidth * canvasScaleFactor;
+	canvas.height = innerHeight * canvasScaleFactor;
+	canvasWidth = innerWidth;
+	canvasHeight = innerHeight;
 
-const ctx = canvas.getContext('2d');
-ctx.scale(canvasScaleFactor, canvasScaleFactor);
+	ctx.fillStyle = '#FFF';
+	ctx.scale(canvasScaleFactor, canvasScaleFactor);
+
+	try {
+		bird.x = canvasWidth * 0.25;
+	} catch (error) {
+		console.info("Passive ReferenceError: Cannot access 'bird' before initialization.");
+	}
+}
+newCanvasSize();
+addEventListener('resize', newCanvasSize);
+
+/* game */
+const bird = {
+	x: canvasWidth * 0.25,
+	y: canvasHeight * 0.25,
+	vy: 0,
+};
+document.addEventListener('click', () => {
+	bird.vy = 10;
+});
+
+let lastTS = 0;
+let FPS = 60;
+let delta = 1;
+function main(ts) {
+	ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+	// calculate the delta multiplier
+	FPS = 1000 / (ts - lastTS) || 60;
+	delta = 60 / FPS;
+	lastTS = ts;
+
+	// bird
+	ctx.fillRect(bird.x, (bird.y -= bird.vy -= 0.5 * delta), 50, 50);
+
+	requestAnimationFrame(main);
+}
+main();
